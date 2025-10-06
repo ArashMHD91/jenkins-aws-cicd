5fd3bf94f6096035890252b4ff62b45a03c000a3
ArashMHD91 <mhdarash91@gmail.com>
2025-10-06 12:15:56 +0200

    Update app version to 5.0

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f"Hello from Jenkins CI/CD! Version: 4.0"  # Changed from 3.0 to 4.0
+    return f"Hello from Jenkins CI/CD! Version: 5.0"  # Changed from 4.0 to 5.0
 
 @app.route('/health')
 def health():
